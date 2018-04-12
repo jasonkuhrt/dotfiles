574a088899e09e6d62c9265f092cfe6b7402d084
Jason Kuhrt <jasonkuhrt@me.com>
2018-04-11 23:23:51 -0400

    hush login

--- force-install.sh
+++ force-install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-# -s makes symbolic links so that changes to configs in .dotfiles 
+# -s makes symbolic links so that changes to configs in .dotfiles
 #    reflect in wherever else they are linked too.
 # -h prevents following links
 # -F Overwrites...
 # -f ...without asking
-# $PWD because a relative source is relative to the _target_. 
+# $PWD because a relative source is relative to the _target_.
 
 ln -shFf $PWD/zshrc ~/.zshrc
 ln -shFf $PWD/ghci ~/.ghci
 ln -shFf $PWD/vimrc ~/.vimrc
+
+# https://stackoverflow.com/questions/15769615/remove-last-login-message-for-new-tabs-in-terminal
+touch ~/.hushlogin
